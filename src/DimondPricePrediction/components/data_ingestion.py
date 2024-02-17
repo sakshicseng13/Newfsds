844ed18d2773ba55254214a1e0f2f069a627250e
Sakshi Shrivastava <sakshi.cseng13@gmail.com>
2024-02-17 20:12:21 +0530

    class DataIngestion

---
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -0,0 +1,14 @@
+
+
+class DataIngestion:
+    def init(self):
+        pass
+
+    def Initiate_DataIngestion(self):
+        pass
+
+    def Get_dataTransformation(self):
+        pass
+
+    def Initiate_Modeltrain(self):
+        pass
