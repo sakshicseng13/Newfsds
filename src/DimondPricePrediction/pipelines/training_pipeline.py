3a846e9d4bfb62ff46c96ce1e6120c36d13f72f0
Sakshi Shrivastava <sakshi.cseng13@gmail.com>
2024-03-08 03:33:23 +0530

    updated

--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -4,7 +4,7 @@ from src.DimondPricePrediction.components.data_transformation import DataTransfo
 
 from src.DimondPricePrediction.components.model_trainer import ModelTrainer
 
-#from src.DimondPricePrediction.components.model_evaluation import ModelEvaluation
+from src.DimondPricePrediction.components.model_evaluation import ModelEvaluation
 
 
 import os
@@ -30,5 +30,7 @@ train_arr,test_arr=data_transformation.initialize_data_transformation(train_data
 model_trainer=ModelTrainer()
 model_trainer.initate_model_training(train_arr,test_arr)
        
-                
+   
+model_eval_obj = ModelEvaluation()
+model_eval_obj.initiate_model_evaluation(train_arr,test_arr)             
    
